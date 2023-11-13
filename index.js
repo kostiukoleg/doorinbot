6473aacf9bc9261072d5f9ba2ae6800f85366199
olegkostiuk <olegkostiuk@ukr.net>
2023-11-13 22:38:34 +0200

    Simple telegram bot for Heroku

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Telegraf } = require('telegraf')
 const { message } = require('telegraf/filters')
 require('dotenv').config()
+const commands = require('./const')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx) => ctx.reply('Welcome'))
