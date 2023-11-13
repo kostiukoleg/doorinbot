948924fcc9d1dc2679fb622cec5e4c60a9a1c32c
olegkostiuk <olegkostiuk@ukr.net>
2023-11-14 00:11:38 +0200

    Add Handle Click

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require('dotenv').config()
 const commands = require('./const')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
+
 bot.start((ctx) => {
   const keyboard = Markup.inlineKeyboard([
     Markup.button.url('Google', 'https://www.google.com'),
@@ -13,6 +14,10 @@ bot.start((ctx) => {
   // Send a message with the inline keyboard
   ctx.reply('Hello! Click the buttons below:', keyboard);
 })
+// Handle button click
+bot.action('button_clicked', (ctx) => {
+  ctx.answerCbQuery('Button clicked!');
+});
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on(message('sticker'), (ctx) => ctx.reply('👍'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
