a799c59920fc895ce7a5b2c6f61d245fcdf2c15e
olegkostiuk <olegkostiuk@ukr.net>
2023-11-14 00:03:34 +0200

    Add key board to telegram bot

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const { Telegraf } = require('telegraf')
+const { Telegraf, Markup } = require('telegraf')
 const { message } = require('telegraf/filters')
 require('dotenv').config()
 const commands = require('./const')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply('Welcome'))
+bot.start((ctx) => {
+  const keyboard = Markup.inlineKeyboard([
+    Markup.button.url('Google', 'https://www.google.com'),
+    Markup.button.callback('Click me', 'button_clicked'),
+  ]);
+
+  // Send a message with the inline keyboard
+  ctx.reply('Hello! Click the buttons below:', keyboard);
+})
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on(message('sticker'), (ctx) => ctx.reply('👍'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
